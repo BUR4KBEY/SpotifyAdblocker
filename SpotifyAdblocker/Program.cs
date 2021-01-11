f21e0ecb7abf1629ef5e982f84c8df9751993d6a
BUR4KBEY <rectfc@gmail.com>
2021-01-11 18:31:51 +0300

    Downloading system simplified

--- SpotifyAdblocker/Program.cs
+++ SpotifyAdblocker/Program.cs
@@ -8,22 +8,37 @@ namespace SpotifyAdblocker
     class Program
     {
         static string currentPath = AppDomain.CurrentDomain.BaseDirectory;
-        static string folderPath = $"{currentPath}data";
+        static string appdataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        static string zipName = "SpotifyAdblocker.zip";
         static void Main(string[] args)
+        => GetSpotifyDirectory();
+
+        static void Next(string spotifyDirectory)
         {
             try
             {
-                string zipPath = $"{currentPath}\\File.zip";
+                Console.Clear();
+                Console.WriteLine($"Spotify Directory: {spotifyDirectory}");
+                Console.WriteLine("Checking files...");
 
-                if (File.Exists(zipPath)) File.Delete(zipPath);
-                if (Directory.Exists(folderPath)) Directory.Delete(folderPath, true);
+                string dllPath = $"{spotifyDirectory}\\chrome_elf.dll";
+                string configPath = $"{spotifyDirectory}\\config.ini";
+
+                if (File.Exists(dllPath)) File.Delete(dllPath);
+                if (File.Exists(configPath)) File.Delete(configPath);
+
+                Console.WriteLine("Download starting...");
 
                 using (WebClient wc = new WebClient())
                 {
+                    string zipPath = $"{spotifyDirectory}\\{zipName}";
                     wc.DownloadFile(new Uri("https://github.com/mrpond/BlockTheSpot/releases/latest/download/chrome_elf.zip"), zipPath);
-                    ZipFile.ExtractToDirectory(zipPath, folderPath);
+                    Console.WriteLine("Extracting...");
+                    ZipFile.ExtractToDirectory(zipPath, spotifyDirectory);
                     File.Delete(zipPath);
-                    GetSpotifyDirectory();
+                    Console.WriteLine("Compleated!");
+                    Console.WriteLine("Successful! You can close this window and run the spotify. Enjoy :)");
+                    Console.ReadKey();
                 }
             }
             catch (Exception ex)
@@ -36,33 +51,15 @@ namespace SpotifyAdblocker
 
         static void GetSpotifyDirectory()
         {
-            try
+            if (File.Exists($"{appdataPath}\\Spotify\\Spotify.exe")) Next($"{appdataPath}\\Spotify");
+            else
             {
                 Console.Write("Please write your spotify directory and be sure the spotify not running: ");
                 string spotifyPath = Console.ReadLine();
                 if (spotifyPath == "" || spotifyPath == string.Empty || spotifyPath == null || spotifyPath.Contains(" ")) GetSpotifyDirectory();
                 else if (!Directory.Exists(spotifyPath)) GetSpotifyDirectory();
                 else if (!File.Exists($"{spotifyPath}\\Spotify.exe")) GetSpotifyDirectory();
-                else
-                {
-                    string chromeDllPath = $"{spotifyPath}\\chrome_elf.dll";
-                    string configIniPath = $"{spotifyPath}\\config.ini";
-
-                    if (File.Exists(chromeDllPath)) File.Delete(chromeDllPath);
-                    if (File.Exists(configIniPath)) File.Delete(configIniPath);
-
-                    File.Move($"{folderPath}\\chrome_elf.dll", $"{spotifyPath}\\chrome_elf.dll");
-                    File.Move($"{folderPath}\\config.ini", $"{spotifyPath}\\config.ini");
-                    Directory.Delete(folderPath, true);
-                    Console.WriteLine("Successful! You can close this window and run the spotify. Enjoy :)");
-                    Console.ReadKey();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.Clear();
-                Console.WriteLine(ex.ToString());
-                Console.ReadKey();
+                else Next(spotifyPath);
             }
         }
     }
